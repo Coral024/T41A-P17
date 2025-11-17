42c8466a0fc2616a6ee6a266dd959d20f37aca7b
Coral024 <202579467+Coral024@users.noreply.github.com>
2025-11-17 14:04:01 -0600

    Implement unit tests for productos_jsonb
    Added unit tests for productos_jsonb table to validate name and color.

--- 04_unit_tests.sql
+++ 04_unit_tests.sql
@@ -25,3 +25,24 @@ BEGIN
   END IF;
 END;
 $$;
+
+--Implementar pruebas unitarias
+DO $$
+BEGIN
+  IF EXISTS (
+    SELECT 1 FROM productos_jsonb WHERE id = 1 AND data->>'nombre' = 'television'
+  ) THEN
+    RAISE NOTICE 'OK: nombre correcto para id 1';
+  ELSE
+    RAISE EXCEPTION 'Fallo: nombre incorrecto para id 1';
+  END IF;
+
+  IF EXISTS (
+    SELECT 1 FROM productos_jsonb WHERE id = 1 AND data->>'color' = 'azul'
+  ) THEN
+    RAISE NOTICE 'OK: usuario color rojo para id 1';
+  ELSE
+    RAISE EXCEPTION 'Fallo: color usuario no es rojo para id 1';
+  END IF;
+END;
+$$;
