664eab790a04c4eea332d037a04dfd5da75e9afb
Coral024 <202579467+Coral024@users.noreply.github.com>
2025-11-17 13:56:14 -0600

    Create tables for usuarios and productos
    Added tables for users and products with JSONB and HSTORE support.

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,4 +1,20 @@
+CREATE EXTENSION IF NOT EXISTS hstore;
+
 CREATE TABLE usuarios (
   id SERIAL PRIMARY KEY,
   data JSONB
+);
+
+CREATE TABLE productos_jsonb(
+  id SERIAL PRIMARY KEY,
+  data JSONB
+);
+
+--Crear una tabla con columna HSTORE
+--Define una tabla productos con atributos como marca, color, peso.
+CREATE TABLE productos_hstore (
+  id SERIAL PRIMARY KEY,
+  nombre TEXT,
+  precio NUMERIC,
+  atributos HSTORE
 );
