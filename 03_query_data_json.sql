e822151edea5ad47dd58389a5180ea0be0e510a4
Coral024 <202579467+Coral024@users.noreply.github.com>
2025-11-17 14:03:31 -0600

    Remove unit tests from 03_query_data_json.sql
    Removed unit test implementation for productos_jsonb.

--- 03_query_data_json.sql
+++ 03_query_data_json.sql
@@ -19,26 +19,7 @@ CREATE INDEX idx_data_gin ON productos_jsonb USING GIN (data);
 SELECT * FROM productos_jsonb
 WHERE data @> '{"categoria": "tecnologia"}';
 
---Implementar pruebas unitarias
-DO $$
-BEGIN
-  IF EXISTS (
-    SELECT 1 FROM productos_jsonb WHERE id = 1 AND data->>'nombre' = 'television'
-  ) THEN
-    RAISE NOTICE 'OK: nombre correcto para id 1';
-  ELSE
-    RAISE EXCEPTION 'Fallo: nombre incorrecto para id 1';
-  END IF;
 
-  IF EXISTS (
-    SELECT 1 FROM productos_jsonb WHERE id = 1 AND data->>'color' = 'azul'
-  ) THEN
-    RAISE NOTICE 'OK: usuario color rojo para id 1';
-  ELSE
-    RAISE EXCEPTION 'Fallo: color usuario no es rojo para id 1';
-  END IF;
-END;
-$$;
 
 
 
